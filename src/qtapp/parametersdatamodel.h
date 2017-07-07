722b3a2584c1947bf37991b627ba7b7ad499ef42
Grégory Beurier <gregory.beurier@cirad.fr>
2017-07-07 09:33:28 +0200

    added edition in paramaters model

--- src/qtapp/parametersdatamodel.h
+++ src/qtapp/parametersdatamodel.h
@@ -15,11 +15,8 @@ public:
     int columnCount(const QModelIndex &parent) const;
     QVariant data(const QModelIndex &index, int role) const;
     QVariant headerData(int section, Qt::Orientation orientation, int role) const;
-//    Qt::ItemFlags flags(const QModelIndex &index) const;
-//    bool setData(const QModelIndex &index, const QVariant &value, int role=Qt::EditRole);
-//    bool insertRows(int position, int rows, const QModelIndex &index=QModelIndex());
-//    bool removeRows(int position, int rows, const QModelIndex &index=QModelIndex());
-//     samara::ModelParameters getParameters();
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    bool setData(const QModelIndex &index, const QVariant &value, int role=Qt::EditRole);
 
 private:
     SamaraParameters * parameters;
